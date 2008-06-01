c0d2d1d7f87bf6be108dd13e0e83904ae1869715
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-06-01 16:21:28 +0000

    Deported functional module integrated.

--- src/code/servers/raw-tcp/tcp_server_test.erl
+++ src/code/servers/raw-tcp/tcp_server_test.erl
@@ -1,6 +1,8 @@
 % Unit tests for the tcp_server.
 % See the tcp_server module.
-% Uses an external test functional module.
+% Uses an external test functional module with an intended bug in it
+% (faulty_functional_module) and replaces it by a corrected one
+% (corrected_test_functional_module).
 -module(tcp_server_test).
 
 
@@ -9,9 +11,6 @@
 
 -define(Tested_modules,[tcp_server]).
 
-
-% Use our functional module dedicated to tests:
--define(functional_module,test_functional_module).
 
 
 % For all facilities common to all tests:
@@ -23,11 +22,15 @@ run() ->
 
 	?test_start,
 	
-	?test_info([ "Creating a new tcp_server." ]),
+	?test_info([ "Creating a new tcp_server, "
+		"starting with faulty functional module." ]),
 
 	% Creates a TCP server and uses an external functional module:
 	ServerPid = tcp_server:start_link( ?test_server_name, local_only, 
-		?functional_module ),
+		faulty_test_functional_module ),
+	
+	?test_info([ "tcp_server state: ~s.", [ 
+		tcp_server:send_request( ServerPid, get_server_state ) ] ]),
 	
 	functionally_initialized = tcp_server:send_request( ServerPid, get_info ),
 	
@@ -39,10 +42,22 @@ run() ->
 
 	?test_info([ "tcp_server behaved correctly." ]),
 	
-	% Uncomment to test failures and transactions:
-	%?test_info([ io_lib:format( "Result of fail_on_purpose: ~w.", 
-	%	[ tcp_server:send_request( ServerPid, fail_on_purpose ) ] ) ]),
+	{request_failed,Message} = tcp_server:send_request( ServerPid,
+		fail_on_purpose ),
+	
+	?test_info([ io_lib:format( "Result of fail_on_purpose: ~w.", 
+		[ Message ] ) ]),
+
+	?test_info([ "Swapping functional implementations: "
+		"replacing faulty one by corrected one." ]),
+	
+	functional_code_swapped = tcp_server:update_functional_code( ServerPid, 
+		corrected_test_functional_module ),
 		
+	% Checks that code has been replaced:	
+	ok = tcp_server:send_request( ServerPid, fail_on_purpose ),
+	
+	% Checks that the state was not lost:	
 	NewInfo = tcp_server:send_request( ServerPid, get_info ),
 	
 	?test_stop.
