845fc2bec30d3bc25df02ad8fae7229aba435fc1
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-06-08 18:40:08 +0000

    Functional module became client manager one, for parallel spawns.

--- src/code/servers/raw-tcp/tcp_server.erl
+++ src/code/servers/raw-tcp/tcp_server.erl
@@ -1,11 +1,10 @@
 % Custom-made TCP/IP server.
 % Features:
-%  - functional behaviour uncoupled from server implementation
+%  - functional behaviour (client management) uncoupled from server
+% implementation
 %  - multiple parallel sessions can be handled
-%  - transaction semantics
 %  - hot code update
 %  - simple state management
-%  - based on PID rather than on registered names
 -module(tcp_server).
 
 
@@ -26,9 +25,12 @@
 
 % The role of these servers is simply to break incoming data stream into
 % appropriate client requests, and to send them updates.
+% For these TCP servers, there is one listening socket, and as many
+% per-connection sockets as clients.
+% TO-DO: limit the max number of simultaneous connections ?
 
 
-% For server defaults:
+% For server defaults and internal record:
 -include("tcp_server.hrl").
 
 
@@ -36,99 +38,103 @@
 -include("traces.hrl").
 
 
-% Record that stores the current state of a TCP server:
--record( server_state, {
-	server_name = undefined,
-	host = undefined,
-	starting_time = undefined,
-	code_update_count = 0,
-	last_code_update = undefined,
-	listening_socket = undefined,
-	listening_port = undefined,
-	current_connections = [],
-	past_connections = []
-} ).
 
 
 % Exported API section.
 
--export([start/3,start_link/3,send_request/2,send_request_by_name/2,
-	update_functional_code/2,state_to_string/1]).
+-export([create/3,create_link/3,send_request/2,send_request_by_name/2,
+	update_code_for_client_management/2,state_to_string/1]).
 
 
-% Starts a new TCP server.
-%  - ServerName the name under which the server will be registered
-%  - RegistrationType is in 'local_only', 'global_only', 'local_and_global', 
-% depending on what kind of registration is requested for this server
-%  - FunctionalModule the Erlang module that will manage the client requests
-% and the pushed updates 
+% Creates a new TCP server.
+%  - ServerName is the server name, under which it will be registered if
+% requested
+%  - RegistrationType is in 'none', 'local_only', 'global_only',
+% 'local_and_global', depending on what kind of registration is requested 
+% for this server
+%  - ClientManagementModule is the Erlang module that will manage the 
+% dialog with clients (each incoming connection will be given a dedicated
+% spawned process running that code)
+%
 % Returns the PID of the launched server.
 % (static)
-start(ServerName,RegistrationType,FunctionalModule) ->
-	?emit_trace([ io_lib:format( "Starting a TCP server named ~s.",
+create( ServerName, RegistrationType, ClientManagementModule ) ->
+
+	?emit_trace([ io_lib:format( "Starting a new TCP server named ~s.",
 		[ ServerName ] ) ]),
-	ServerPid = spawn( fun() ->	init( ServerName, FunctionalModule ) end ),
-	ok = utils:register_as( ServerPid, ServerName, RegistrationType ),
-	ServerPid.
-
-
-% Starts a new TCP server, linked to the calling process.
-%  - ServerName the name under which the server will be registered
-%  - RegistrationType is in 'local_only', 'global_only', 'local_and_global', 
-% depending on what kind of registration is requested for this server
-%  - FunctionalModule the Erlang module that will manage the client requests
-% and the pushed updates 
-% Returns the PID of the launched server.
-% (static)
-start_link(ServerName,RegistrationType,FunctionalModule) ->
-	?emit_trace([ io_lib:format( "Starting a linked TCP server named ~s.",
-		[ ServerName ] ) ]),
-	ServerPid = spawn_link( 
-		fun() -> init( ServerName, FunctionalModule ) end ),
-	ok = utils:register_as( ServerPid, ServerName,RegistrationType ),
-	ServerPid.
-
-
-% Initializes the TCP server with specified name and functional module, and
-% enter its main loop.
-% A default TCP port is used here.
-init( ServerName, FunctionalModule ) ->
-	init( ServerName, FunctionalModule, ?default_listening_tcp_server_port ).
-
-
-% Initializes the TCP server with specified name, functional module and TCP
-% port, and enter its main loop.
-init( ServerName, FunctionalModule, ListeningTCPPort ) ->
-
-	?emit_debug([ io_lib:format( "Server will listen to TCP port ~B.", 
-		[ListeningTCPPort] ) ]),
+	
+	% Avoids to export the init function:
+	spawn( fun() -> init( ServerName,RegistrationType,ClientManagementModule)
+		end ).
 		
-	% All network interfaces listen to, 32-bit header:
+
+% Creates a new linked TCP server.
+%  - ServerName is the server name, under which it will be registered if
+% requested
+%  - RegistrationType is in 'none', 'local_only', 'global_only',
+% 'local_and_global', depending on what kind of registration is requested 
+% for this server
+%  - ClientManagementModule is the Erlang module that will manage the 
+% dialog with clients (each incoming connection will be given a dedicated
+% spawned process running that code)
+%
+% Returns the PID of the launched server.
+% (static)
+create_link( ServerName, RegistrationType, ClientManagementModule ) ->
+
+	?emit_trace([ io_lib:format( "Starting a new linked TCP server named ~s.",
+		[ ServerName ] ) ]),
+			
+	% Avoids to export the init function:
+	spawn_link( fun() -> 
+		init( ServerName,RegistrationType,ClientManagementModule) end ).
+
+
+
+% Initializes the TCP server with specified name and module for client 
+% management, and enters its main loop, listening for incoming connections.
+% A default TCP port is used here.
+init( ServerName, RegistrationType, ClientManagementModule ) ->
+	init( ServerName, RegistrationType, ClientManagementModule, 
+		?default_listening_tcp_server_port ).
+
+
+% Initializes the TCP server with specified name, module for client 
+% management and TCP port, and enters its main loop, listening for incoming
+% connections.
+init( ServerName, RegistrationType, ClientManagementModule, 
+		ListeningTCPPort ) ->
+
+	ok = utils:register_as( ServerName, RegistrationType ),
+
+	?emit_debug([ io_lib:format( "Server will listen to TCP port ~B, "
+		"with a maximum backlog of ~B.", 
+		[ListeningTCPPort,?default_backlog] ) ]),
+		
+	% Listen to all network interfaces:
 	{ok,ListenSocket} = gen_tcp:listen( ListeningTCPPort, [
-		binary, {packet,4}, {backlog,?default_backlog}, 
-		{reuseaddr,true}, {active,true} ] ),
+		binary, {backlog,?default_backlog},	{reuseaddr,true} ] ),
 
 	?emit_debug([ "Server waiting for incoming connections." ]),
-		
-	% Will block until a connection happens:
-	{ok,ClientSocket} = gen_tcp:accept(ListenSocket),
-	
-	?emit_debug([ "Connection accepted." ]),
-		
+
 	ServerState = #server_state{
 		server_name = ServerName,
 	    host = net_adm:localhost(),
 	    starting_time = utils:get_timestamp(),
-	    code_update_count = 0,
-	    last_code_update = undefined,
+		current_client_module = ClientManagementModule,
+	    client_code_update_count = 0,
+	    last_client_code_update = undefined,
 		listening_socket = ListenSocket,
 	    listening_port = ListeningTCPPort,
-	    current_connections = [ClientSocket],
-	    past_connections = []
+		accepted_connections = [],
+		closed_connections = []
 	},
-	FunctionalState = FunctionalModule:init(),
-	loop( ServerState, FunctionalModule, FunctionalState ).
-	
+	loop( ServerState ).
+
+
+
+
+% Client API.
 
 % Sends specified request to specified PID or name registered locally.
 send_request( ServerPid, Request ) when is_pid(ServerPid) ->
@@ -150,8 +156,8 @@ send_request_by_name( ServerName, Request ) ->
 
 % Replaces, for specified server, current version of functional code by
 % specified one.
-update_functional_code(ServerPid,NewFunctionalModule) ->
-	send_request( ServerPid, {update_functional_code,NewFunctionalModule} ).
+update_code_for_client_management(ServerPid,NewClientManagementModule) ->
+	send_request( ServerPid, {update_code_for_client_management,NewClientManagementModule} ).
 
 
 
@@ -162,60 +168,58 @@ update_functional_code(ServerPid,NewFunctionalModule) ->
 % Main loop of this server.
 %  - ServerState is the current state of this server (ex: containing the name
 % under which the server is registered)
-%  - FunctionalModule the Erlang module that manages the client requests
-% and the pushed updates
-%  - FunctionalState the state of the functional server 
-loop( ServerState, FunctionalModule, FunctionalState ) ->
+loop( ServerState ) ->
+
 	?emit_debug([ io_lib:format( "~s~n", [ state_to_string(ServerState) ] ) ]),
+
+	?emit_trace([ "Spawning a client manager for next connection." ]),
+
+	% A pool of more than one manager waiting for accept could also be used:
+	NewClientManagerPid = spawn( ?getState.current_client_module,
+		init, [	self(), ?getState.listening_socket ] ),
+	
 	receive
 	
-		{tcp,ClientSocket,BinMessage} ->
-			?emit_debug([ io_lib:format( "Server received binary ~p.~n",
-				[BinMessage] ) ]),
-			gen_tcp:send( ClientSocket, term_to_binary(packet_received) ),
-			?emit_debug([ "Server answered.~n" ]),
-			loop( ServerState, FunctionalModule, FunctionalState );
+		{NewClientManagerPid,accepted} ->
+			loop( on_client_connection( ServerState, NewClientManagerPid ) );
+					
+		{AClientManagerPid,closed} ->
+			loop( on_client_disconnection( ServerState, AClientManagerPid ) )
+										
+	end.
 
-		{tcp_closed,_ClientSocket} ->
-			?emit_trace([ "Client closed its socket.~n" ]);
-		
-		{From,{update_functional_code,NewFunctionalModule}} ->
-			From ! {self(),functional_code_swapped},
-			loop( ServerState#server_state{
-					code_update_count =
-						ServerState#server_state.code_update_count + 1,
-					last_code_update = utils:get_timestamp() 
-				},
-				NewFunctionalModule, FunctionalState );
 
-		{From,get_server_state} ->
-			From ! {self(),state_to_string(ServerState)},
-			loop( ServerState, FunctionalModule, FunctionalState );
+
+% Returns an updated server state.
+on_client_connection( ServerState, ClientManagerPid ) ->
+
+	?emit_debug([ io_lib:format( 
+		"New connection: registering client manager ~w.",
+		[ClientManagerPid] ) ]),
 	
-		{From,Request} ->
-			try FunctionalModule:handle( Request, FunctionalState ) of
-			
-				{Answer,NewFunctionalState} ->
-					From ! {self(),Answer},
-					loop( ServerState, FunctionalModule, NewFunctionalState )
-			
-			catch
-			
-				_:Why ->
-					?emit_error([ io_lib:format(
-						"Server whose state is ~w failed "
-						"while executing request ~w for ~w: ~w.",
-						[ServerState,Request,From,Why] ) ]),
-						
-					% Notify the client:	
-					From ! {self(),request_failed,Why},
-					
-					% Transaction: loop with initial state (no change):
-					loop( ServerState, FunctionalModule, FunctionalState )
-					
-			end				
+	?getState{ accepted_connections =
+		[ ClientManagerPid | ?getState.accepted_connections ] }.
+
+
+% Returns an updated server state.
+on_client_disconnection( ServerState, ClientManagerPid ) ->
+	CurrentConnections = ?getState.accepted_connections,
+	case lists:member( ClientManagerPid, CurrentConnections ) of
+	
+		true ->
+			?emit_debug([ io_lib:format( 
+				"Closed connection: removing client manager ~w.",
+				[ClientManagerPid] ) ]),
+			?getState{ accepted_connections =
+				lists:delete( ClientManagerPid, CurrentConnections ) };
+				
+		false ->
+			?emit_error([ io_lib:format( "Error, closed connection ~w "
+				"was not a registered one, nothing done.",
+				[ClientManagerPid] ) ])
 	
 	end.
+	
 
 
 
@@ -226,17 +230,19 @@ state_to_string(ServerState) ->
 		"  + started on ~s at ~s~n"
 		"  + listening to TCP port ~w~n"
 		"  + ~s~n"
-		"  + current connections: ~w~n"
-		"  + past connections: ~w~n",
+		"  + current client management module: ~w~n"
+		"  + accepted (current) connections: ~w~n"
+		"  + closed connections: ~w~n",
 		[ 
-			ServerState#server_state.server_name,
-			ServerState#server_state.host,
+			?getState.server_name,
+			?getState.host,
 			utils:get_textual_timestamp(
-				ServerState#server_state.starting_time), 
-			ServerState#server_state.listening_port, 
+				?getState.starting_time), 
+			?getState.listening_port, 
+			?getState.current_client_module,
 			code_updates_to_string(ServerState),
-			ServerState#server_state.current_connections,
-			ServerState#server_state.past_connections
+			?getState.accepted_connections,
+			?getState.closed_connections
 		] ).	
 
 
@@ -244,16 +250,16 @@ state_to_string(ServerState) ->
 % Returns a textual description of past code updates.
 code_updates_to_string(ServerState) ->
 
-	case ServerState#server_state.last_code_update of 
+	case ?getState.last_client_code_update of 
 		
 		undefined ->
-			"no functional code update performed yet" ;
+			"no update of client management code performed yet" ;
 			
 		TimeStampPair ->
-			io_lib:format( "~B functional code update(s) performed, "
+			io_lib:format( "~B update(s) of client management code performed, "
 				"last one was at ~s",
 				[ 
-					ServerState#server_state.code_update_count,
+					?getState.client_code_update_count,
 					utils:get_textual_timestamp(TimeStampPair) 
 				] )
 				
