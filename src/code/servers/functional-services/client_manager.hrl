5532cea7e49ed3ee7b789b49800e2761bb0aca88
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-06-28 18:56:05 +0000

    manager_state updated.

--- src/code/servers/functional-services/client_manager.hrl
+++ src/code/servers/functional-services/client_manager.hrl
@@ -3,10 +3,11 @@
 % Internal record that stores the current state of the server-side manager of 
 % a client connection:
 -record( manager_state, {
-	client_name = undefined,
-	client_host = undefined,
+	client_login  = undefined,
+	client_host   = undefined,
 	starting_time = undefined,
-	client_socket = undefined
+	client_socket = undefined,
+	server_pid    = undefined
 } ).
 
 
