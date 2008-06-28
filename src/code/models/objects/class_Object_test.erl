33fc57c791ec55a1c15ada8aba636274c86774fb
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-06-28 18:53:46 +0000

    getSize and other methods tested.

--- src/code/models/objects/class_Object_test.erl
+++ src/code/models/objects/class_Object_test.erl
@@ -19,64 +19,38 @@ run() ->
 	?test_start,
 		
 	?test_info([ "Creating a new test Object." ]),
+			
+	Size = 0.8,
+			
+	MyObject = class_Object:new_link( "Bottle of Whisky", Size, 1.1, 120 ),	
 	
-	MyLocation = #location{ x = 1, y = 2, z = 3 },
-		
-	MyObject = class_Object:new_link( MyLocation ),		
-	
-	MyObject ! {getLocation,[],self()},
-	
+	MyObject ! {getSize,[],self()},
 	receive
 	
-		{wooper_result,MyLocation} ->
-				?test_info([ "getLocation succeeded." ])
+		{wooper_result,Size} ->
+				?test_info([ "getSize succeeded." ])
 
 	end,
 	
-	Origin = space:origin(),
+	NewSize = 0.9,
 	
-	?test_info([ io_lib:format("Origin is ~w.", [ Origin ]) ]),
+	MyObject ! {setSize,NewSize},
 	
-	MyObject ! {setLocation,Origin},
-	
-	MyObject ! {getLocation,[],self()},
-
+	MyObject ! {getSize,[],self()},
 	receive
 	
-		{wooper_result,Origin} ->
-				?test_info([ "setLocation succeeded." ]);
-
-		{wooper_result,Other} ->
-				?test_error([ 
-					io_lib:format("setLocation failed: ~w.", [ Other ]) ])
-
-	end,
-	
-	Abscissa = 7,
-	MyObject ! {setAbscissa,Abscissa},
-	MyObject ! {getAbscissa,[],self()},
-	
-	receive
-	
-		{wooper_result,Abscissa} ->
-				?test_info([ "setAbscissa and getAbscissa succeeded." ])
+		{wooper_result,NewSize} ->
+				?test_info([ "setSize succeeded." ])
 
 	end,
 
-	Ordinate = 17,
-	Altitude = 22,
-
-	MyObject ! {setOrdinate,Ordinate},
-	MyObject ! {setAltitude,Altitude},
-	
-	FinalLocation = #location{x=Abscissa,y=Ordinate,z=Altitude},
-
-	MyObject ! {getLocation,[],self()},
+	MyObject ! {toString,[],self()},
 	receive
 	
-		{wooper_result,FinalLocation} ->
-				?test_info([ "set/get for abscissa, ordinate and altitude "
-					"succeeded." ])
+		{wooper_result,StateString} ->
+				?test_info([ io_lib:format( 
+					"Received following full textual description: ~s",
+					[StateString] ) ])
 
 	end,
 	
