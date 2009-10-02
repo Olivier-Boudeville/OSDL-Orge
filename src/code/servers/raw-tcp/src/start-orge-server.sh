017c0550db43e573470dee76c8950d3c5a9ed83f
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-10-02 08:01:23 +0000

    epmd better managed, commnets improved.

--- src/code/servers/raw-tcp/src/start-orge-server.sh
+++ src/code/servers/raw-tcp/src/start-orge-server.sh
@@ -5,7 +5,8 @@ previous_server_pid=`ps -edf|grep beam|grep orge_tcp_server_exec_background|awk 
 
 if [ -n "$previous_server_pid" ] ; then
 
-	echo "An already running Orge server was detected (PID $previous_server_pid), you must kill it first.\n Example: 'kill -9 $previous_server_pid'" 1>&2
+	echo "An already running Orge server was detected (PID $previous_server_pid), you must kill it first.
+  Example: 'kill -9 $previous_server_pid'" 1>&2
 	
 	exit 1
 	
@@ -13,6 +14,9 @@ fi
 
 
 echo "Starting Orge server..."
+
+# Needed to launch an EPMD daemon on a non-standard port:
+epmd -daemon -port 4269
 
 make orge_tcp_server_exec_background 
 
