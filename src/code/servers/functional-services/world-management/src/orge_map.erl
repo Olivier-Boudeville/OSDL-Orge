b1ffe28313bb14294cd306e206e424ca6df53041
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-02-14 22:54:40 +0000

    Working on the convex hull.

--- src/code/servers/functional-services/world-management/src/orge_map.erl
+++ src/code/servers/functional-services/world-management/src/orge_map.erl
@@ -34,9 +34,19 @@
 		  get_upright_square/0, get_upright_square/2, render_polygon/2 ]).
 
 
+% Camera management.
+-export([ set_camera/3, set_current_camera/2 ]).
 
+
+% Bounding-box management.
+% First algorithm to find the smallest enclosing circle of a polygon was based
+% on compute_max_overall_distance/2 bud sadly was wrong (ex: failing with a 
+% isosceles triangle.
+-export([ compute_max_overall_distance/2 ]).
 
 % A 2D point is simply a pair of integers, ex: P = {3,45}.
+
+% Identifiers are generally atoms.
 
 
 % Describes a 2D polygon, convex or not.
@@ -49,10 +59,91 @@
 		   edge_color = black,
 
            % Color of the inside:
-           fill_color = grey
+           fill_color = grey,
+
+		   % Bounding-box information:
+
+		   % Center of the bounding-box:
+		   center,
+
+		   % Radius of the bounding-box:
+		   radius
+
 		  } 
 
 	   ).
+
+
+
+% Describes a camera in the virtual world.
+-record( camera, {
+
+		   location = {0,0},
+		   color = red,
+		   name
+
+		  }
+	   ).
+
+
+
+% Describes a virtual world.   
+-record( world, {
+		   
+		   % Overall rectangular dimensions ({width,height}):
+		   dimensions,
+
+		   % World elements:
+		   polygons = [],
+
+		   % Known cameras ({camera_id,Camera} pairs):
+		   cameras = [],
+
+		   % Current camera (camera_id):
+		   current_camera,
+
+		   locations
+
+		  } 
+	   ).
+
+
+
+% Describes the current state of a GUI.
+-record( gui_state, {
+
+		   world,
+
+		   main_win,
+
+		   canvas
+
+		  }
+	   ).
+
+
+
+% Point section
+
+
+% Returns the square of the distance between the two specified points.
+% For comparison purposes, computing the square root is useless.
+square_distance( {X1,Y1}, {X2,Y2} ) ->
+	XDiff = X2-X1,
+	YDiff = Y2-Y1,
+	XDiff*XDiff + YDiff*YDiff.
+
+
+% Returns the cross-product of the two specified 2D points, i.e. the magnitude
+% of the vector that would result from a regular 3D cross product of the input
+% vectors, taking their Z values implicitly as 0.
+cross_product( {X1,Y1}, {X2,Y2} ) ->
+	X1*Y2 - Y1*X1.
+
+
+% Returns a vertex corresponding the middle of the two specified vertices.
+get_center( {X1,Y1}, {X2,Y2} ) ->
+	{ erlang:round( (X1+X2)/2 ), erlang:round( (Y1+Y2)/2 ) }.
 
 
 
@@ -104,37 +195,167 @@ draw_cross( _Location = {X,Y}, EdgeLength, Color, Canvas ) ->
 	draw_line( {X,Y-Offset}, {X,Y+Offset+1}, Color, Canvas ).
 
 
+
+
+% Bounding-box section.
+
+
+% Renders specified bounding-box.
+render_bounding_box( Center = {XCenter,YCenter}, Radius, Canvas ) ->
+	TopLeft     = {XCenter-Radius, YCenter-Radius},
+	BottomRight = {XCenter+Radius, YCenter+Radius},
+	draw_cross( Center, _EdgeLength = 4, Canvas ),
+	gs:create( oval, Canvas, [ {coords,[TopLeft,BottomRight]},
+									   {fill,none}, {bw,1} ] ).	
+		
+
 % Polygon section.
 
 
 % Returns an example triangle. 
 get_triangle() ->
-	get_triangle( {10,10}, {50,15}, {40,35} ).
+	get_triangle( {110,110}, {250,155}, {120,335} ).
 
 
 % Returns a triangle corresponding to the specified three vertices.
 get_triangle( V1, V2, V3 ) ->
-	#polygon{ vertices = [V1,V2,V3] }.
+	update_diameter( #polygon{ vertices = [V1,V2,V3] } ).
 
 
 % Returns an example upright square.
 get_upright_square() -> 
-	get_upright_square( _Center = {50,50}, _EdgeLength = 20 ).
+	get_upright_square( _Center = {250,250}, _EdgeLength = 50 ).
 
 
-% Returns an upright square corresponding to the specified center and edge length.
+% Returns an upright square corresponding to the specified center and edge 
+% length.
 get_upright_square( _Center = {Xc,Yc}, EdgeLength ) ->
-	Offset = EdgeLength / 2,
+	Offset = erlang:round(EdgeLength / 2),
 	X1 = Xc - Offset,
 	X2 = Xc + Offset,
 	Y1 = Yc - Offset,
 	Y2 = Yc + Offset,
-	#polygon{ vertices = [ {X1,Y1}, {X2,Y1}, {X2,Y2}, {X1,Y2} ] }.
+	update_diameter( #polygon{ vertices = 
+							   [ {X1,Y1}, {X2,Y1}, {X2,Y2}, {X1,Y2} ] } ).
+
+
+
+% Returns a polygon diameter, i.e. two points in the polygon which are at the
+% maximum distance one of the other.
+% Returns {V1,V2,  D = square_distance(V1,V2)} when V1 and V2 are the endpoints
+% of a diameter and D is its square length. 
+get_diameter( Polygon ) ->
+	case Polygon#polygon.vertices of
+
+		[] ->
+			throw( no_vertex );
+
+		[_Vertex] ->
+			throw( single_vertex );
+
+		ListWithAtLeastTwoVertices ->
+			% There are at least two vertices:
+			%compute_max_overall_distance( ListWithAtLeastTwoVertices, 
+			%							  _Longest = undefined )
+			compute_smallest_enclosing_rectangle( ListWithAtLeastTwoVertices, 
+												  undefined, undefined )
+	
+	end.
+
+
+% Returns {V1,V2,square_distance(V1,V2)} so that (square) distance is maximal.
+% We ensure that each edge is examined only once: when the distance between 
+% a given vertex V and all other vertices have been computed, V is removed from
+% the list and a new maximum is searched within this subset.
+% Here there is only one vertex left:
+compute_max_overall_distance( [_H], Longest ) ->
+	Longest;
+
+% Here we have not compute a distance yet:
+compute_max_overall_distance( [H|Others], undefined ) ->
+	FirstEntry = compute_max_distance_between( H, Others, undefined ),
+	compute_max_overall_distance( Others, FirstEntry );
+ 
+% At least one other vertex remains, and at least one distance was computed:
+compute_max_overall_distance( [H|Others], 
+							  Best = {_V1,_V2,LongestSquareDistance} ) ->
+
+	case compute_max_distance_between( H, Others, undefined ) of
+
+		{PmaxForH,LongestSquareDistanceFromH} when LongestSquareDistanceFromH >
+												   LongestSquareDistance ->
+			compute_max_overall_distance( Others, 
+			   {H,PmaxForH,LongestSquareDistanceFromH} );
+										   
+		% Here LongestSquareDistance is not beaten:
+		_Other ->							 
+			compute_max_overall_distance( Others, Best )
+				
+	end.
+
+
+
+% Computes the maximum distance between a vertex (V1) and a list of other vertices.
+% Returns {V1,Vmax,LongestSquareDistance} with LongestSquareDistance being
+% the distance between V1 and Vmax, Vmax being chosen so that 
+% LongestSquareDistance is maximal.
+% As there must have been at least one vertex in the list, Vmax exists here
+% (never undefined):
+compute_max_distance_between( V1, [], {Vmax,LongestSquareDistance} ) ->
+	{V1,Vmax,LongestSquareDistance};
+
+compute_max_distance_between( V1, [V2|OtherVertices], undefined ) ->
+	% First vertex examined is at first by construction the first best:
+	compute_max_distance_between( V1, OtherVertices, 
+								  {V2,square_distance(V1,V2)} );
+
+compute_max_distance_between( V1, [V2|OtherVertices],
+							  {Vmax,LongestSquareDistance} ) ->
+
+	case square_distance(V1,V2) of
+
+		SquareDistance when SquareDistance > LongestSquareDistance ->
+			% We have a new winner:
+			compute_max_distance_between( V1, OtherVertices, 
+										  {V2,SquareDistance} );
+
+		_LesserSquareDistance ->
+			% Previous best not beaten, let's keep it:
+			compute_max_distance_between( V1, OtherVertices, 
+										  {Vmax,LongestSquareDistance} )
+	end.
+
+
+% Computes the smallest rectangle that encloses the specified list of points.
+% Returns { TopLeft, BottomRight }.
+compute_smallest_enclosing_rectangle( [], TopLeft, BottomRight ) ->
+	{ TopLeft, BottomRight, square_distance(TopLeft,BottomRight) };
+
+compute_smallest_enclosing_rectangle( [P|Others], undefined, undefined ) ->
+	% First found initializes best:
+	compute_smallest_enclosing_rectangle( Others, _TopLeft = P, _BottomRight = P );
+
+compute_smallest_enclosing_rectangle( [{X,Y}|Others], {Xt,Yt}, {Xb,Yb} ) ->
+	Xmin = erlang:min( X, Xt ),
+	Ymin = erlang:min( Y, Yt ),
+	Xmax = erlang:max( X, Xb ),
+	Ymax = erlang:max( Y, Yb ),											
+	compute_smallest_enclosing_rectangle( Others, {Xmin,Ymin}, {Xmax,Ymax} ).
+
+
+% Updates the precomputed diameter of the specified polygon.
+% Returns an updated polygon.
+update_diameter( Polygon ) ->
+	{V1,V2,D} = get_diameter( Polygon ),
+	Polygon#polygon{ center = get_center( V1, V2 ), 
+					 radius = erlang:round( math:sqrt(D)/2 ) }.
+
 
 
 % Renders specified polygon in specified canvas.
 % Throws an exception if the polygon is not valid.
 render_polygon( Polygon, Canvas ) ->
+	io:format( "Rendering polygon:~n~s.~n", [polygon_to_string(Polygon)] ),
 	case Polygon#polygon.vertices of
 
 		[] ->
@@ -146,7 +367,13 @@ render_polygon( Polygon, Canvas ) ->
 		Vertices ->
 			gs:create( polygon, Canvas, [ {coords,Vertices},
 										  {fg, Polygon#polygon.edge_color},
-										  {fill, Polygon#polygon.fill_color} ] )
+										  {fill, Polygon#polygon.fill_color} ] 
+					 ),
+
+			% Bounding-box is a circle:
+			render_bounding_box( Polygon#polygon.center, 
+								 Polygon#polygon.radius, Canvas )
+
 %% 		Vertices = [First|_OtherVertices] ->
 %% 			EdgeColor = Polygon#polygon.edge_color,
 %% 			draw_lines( [First|lists:reverse(Vertices)], EdgeColor, Canvas )
@@ -156,18 +383,101 @@ render_polygon( Polygon, Canvas ) ->
 
 
 
+
+% Returns a textual description of the specified polygon.
+polygon_to_string( Polygon ) ->
+	io_lib:format(  "  + vertices: ~w~n", [Polygon#polygon.vertices] )
+		++ io_lib:format( "  + edge color: ~w~n", [Polygon#polygon.edge_color] )
+		++ io_lib:format( "  + fill color: ~w~n", [Polygon#polygon.fill_color] )
+		++ io_lib:format( "  + BB center: ~w~n", [Polygon#polygon.center] )
+		++ io_lib:format( "  + BB radius: ~w~n", [Polygon#polygon.radius] ).
+
+
+
+
+% Convex Hull section.
+
+
+% Finds the pivot, i.e. the leftmost point with the highest ordinate.
+% The point list is supposed not having duplicates.
+find_pivot( _PointList = [Pivot|Others] ) ->
+	find_pivot( Others, Pivot ).
+
+
+find_pivot( [], Pivot ) ->
+	Pivot;
+
+% Higher than the pivot, thus not wanted:
+find_pivot( [{_X,Y}|Others], Pivot={_Xp,Yp} ) when Y<Yp ->
+	find_pivot( Others, Pivot );
+
+% Lower than the pivot, thus wanted:
+find_pivot( [Point={_X,Y}|Others], _Pivot={_Xp,Yp} ) when Y>Yp ->
+	find_pivot( Others, Point );
+
+% Same level as the pivot, but at its right, thus not wanted:
+find_pivot( [Point={X,Yp}|Others], Pivot={Xp,Yp} ) when X>Xp ->
+	find_pivot( Others, Pivot );
+
+% Same level as the pivot, but at its left, thus wanted:
+find_pivot( [Point={X,Yp}|Others], Pivot={Xp,Yp} ) when X<Xp ->	
+	find_pivot( Others, Point );
+
+% Duplicated pivot, abnormal:
+find_pivot( [Pivot|_Others], Pivot ) ->
+	throw( {duplicate_pivot,Pivot} ).
+
+
+sort_by_angle( Pivot, Points ) ->
+	% Each element of that list is {Cotangent,P}, where Cotangent is the
+	% cotangent of the angle the lines defined by the pivot and P makes with
+	% the x axis.
+	PointsWithAngles = [ {angle_value(P,Pivot),P} || P <- Points ],
+	SortedPoints = lists:keysort(1,PointsWithAngles)
+
+
+% Returns a vector V made from the specified two points: V=P2-P1.
+vectorize( _P1={X1,Y1}, _P2={X2,Y2} ) ->
+	{X2-X1,Y2-Y1}.
+
+
+% Returns the square of the magnitude of the specified vector.
+square_magnitude( _V={X,Y} ) ->
+	X*X+Y*Y.
+
+
+% Returns the magnitude of the specified vector.
+magnitude( V ) ->
+	math:sqrt( square_magnitude(V) ).
+
+
+% Scales specified vector of specified factor.
+scale( _V={X,Y}, Factor ) ->
+	{Factor*X,Factor*Y}.
+
+
+% Returns the specified vector with an unit length (magnitude of 1):
+make_unit( {0,0} ) ->
+	throw( cannot_make_null_vector_unit );
+
+make_unit( V ) ->
+	scale( V, 1/magnitude(V) ).
+
+
+
+
+
+
+
+
+% Returns a value monotonically increasing with the angle the vector P1P2 
+% makes with the abscissa axis, knowing it is in [0,Pi[.
+angle_value( P1, P2 ) ->
+	V = make_unit( vectorize( P1, P2 ) ),
+	% Let a be the angle between [1,0] and V={X,Y}: tan(a)= Y/X
+
 % Camera section.
 
-
-% Describes a camera in the virtual world.
--record( camera, {
-
-		   location = {0,0},
-		   color = red,
-		   name
-
-		  }
-	   ).
 
 
 % Sets the location of specified camera.
@@ -184,30 +494,83 @@ render_camera( Camera, Canvas ) ->
 
 
 % Returns a default camera.
-get_default_camera() ->
-	#camera{ name = "Default camera"}.
+create_default_camera() ->
+	#camera{ name = "Default" }.
 
 
+
+% Returns the specified camera.
+get_camera( GuiState, CameraId ) ->
+
+	World = get_world(GuiState),
+
+	case lists:keyfind( CameraId, _Pos=1, World#world.cameras ) of
+
+		false -> 
+			throw( {camera_not_found,CameraId} );
+
+		{CameraId,Camera} ->
+			Camera
+
+	end.
+
+
+
+% Returns the current camera, if any.
+get_current_camera( GuiState ) ->
+
+	World = get_world(GuiState),
+
+	case World#world.current_camera of
+
+		undefined ->
+			throw( no_current_camera );
+
+		CurrentCameraId ->
+			get_camera(GuiState,CurrentCameraId)
+
+	end.
+
+
+
+% Sets specified camera identifier to specified camera state, possibly updating
+% a previously existing camera pair.
+% Returns an updated GUI state.
+set_camera( CameraId, Camera, GuiState ) ->
+
+	World = get_world(GuiState),
+
+	NewCameras = lists:keyreplace( CameraId, _Pos=1,  World#world.cameras, 
+								   {CameraId,Camera} ),
+	
+	GuiState#gui_state{ world = World#world{ cameras = NewCameras } }.
+
+
+
+% Sets current camera, possibly updated the previously current camera.
+% Returns an updated GUI state.
+set_current_camera( Camera, GuiState ) ->
+
+	World = get_world(GuiState),
+
+	case  World#world.current_camera of
+
+		undefined ->
+			throw( no_current_camera );
+
+		CurrentCameraId ->
+			set_camera( CurrentCameraId, Camera, GuiState )
+
+	end.
 
 
 
 % World section.
 
 
-% Describes a virtual world.   
--record( world, {
-		   
-		   % Overall rectangular dimensions ({width,height}):
-		   dimensions,
-
-		   % World elements:
-		   polygons = [],
-
-		   % Known cameras:
-		   cameras = []
-
-		  } 
-	   ).
+% Returns the world recorded in the specified GUI state.
+get_world( GuiState ) ->
+	GuiState#gui_state.world.
 
 
 % Creates an example world.
@@ -219,13 +582,20 @@ create_example_world() ->
 	WorldDimensions = {WorldWidth,WorldHeight},
 
 	% Centers the default camera:
-	Camera = set_camera_location( {WorldWidth div 2,WorldHeight div 2},
-								  get_default_camera() ),
+	DefaultCamera = set_camera_location( {WorldWidth div 2,WorldHeight div 2},
+								  create_default_camera() ),
+
+	RandomPoints = [ {random:uniform(300)+500,random:uniform(300)+200}
+					 || _Count <- lists:seq(1,20) ],
+
+	io:format( "Random points: ~w.~n", [RandomPoints] ),
 
 	#world{ 
 			dimensions = WorldDimensions,
 			polygons = [ get_triangle(), get_upright_square() ],
-			cameras  = [ Camera ] 
+			cameras  = [ {default_camera,DefaultCamera} ],
+			current_camera = default_camera,
+			locations = RandomPoints
 		  }.
 
 
@@ -233,8 +603,13 @@ create_example_world() ->
 % Renders specified world in specified canvas.
 render_world( World, Canvas ) ->
 	[ render_polygon( P, Canvas ) || P <- World#world.polygons ],
-	[ render_camera(  C, Canvas ) || C <- World#world.cameras ].
+	[ render_camera(  Cam, Canvas ) || {_CamId,Cam} <- World#world.cameras ],
+	[ draw_cross( Loc, Canvas ) || Loc <- World#world.locations ],
 
+	Pivot = find_pivot( World#world.locations ),
+
+	io:format( "Pivot: ~w.~n", [Pivot] ),
+	draw_cross( Pivot, 10, blue, Canvas ).
 
 
 
@@ -258,13 +633,6 @@ get_canvas_height() ->
 	600.
 
 
-% Describes the current state of a GUI.
--record( gui_state, {
-		   world,
-		   main_win,
-		   canvas
-		  }
-	   ).
 
 
 % Creates the initial GUI.
@@ -288,6 +656,7 @@ create_initial_gui( GsId ) ->
 	%   - first row: empty then canvas
 	%   - second row: camera panel then empty
 	gs:frame( packer_win, MainWin, [ 
+      {bw,1},
 	  {packer_x,[ {fixed,CameraPanelWidth}, {stretch,10} ]},
 	  {packer_y,[ {stretch,10},{fixed,CameraPanelHeight} ]}
 							   ] ),	
@@ -314,14 +683,23 @@ create_initial_gui( GsId ) ->
 	  {width,CameraPanelWidth},
 	  {height,CameraPanelHeight} ] ),
 
-	gs:label( packer_cam, [ {label,{text,"Camera position"}},
-						 {pack_xy,{{1,3},1}} ]),
+	gs:create( label, cam_label, packer_cam, [ {label,{text,"No camera set"}},
+      {justify,center}, {bw,1}, {pack_xy,{{1,3},1}} ]),
 
-	gs:button( packer_cam, [ {label,{text,"Left"}},   {pack_xy,{1,3}} ]),
-	gs:button( packer_cam, [ {label,{text,"Right"}},  {pack_xy,{3,3}} ]),
-	gs:button( packer_cam, [ {label,{text,"Center"}}, {pack_xy,{2,3}} ]),
-	gs:button( packer_cam, [ {label,{text,"Up"}},     {pack_xy,{2,2}} ]),
-	gs:button( packer_cam, [ {label,{text,"Down"}},   {pack_xy,{2,4}} ]),
+	gs:create( button, cam_left_button, packer_cam, 
+      [ {label,{text,"Left"}}, {pack_xy,{1,3}} ]),
+
+	gs:create( button, cam_right_button, packer_cam, 
+	  [ {label,{text,"Right"}}, {pack_xy,{3,3}} ]),
+
+	gs:create( button, cam_center_button, packer_cam, 
+      [ {label,{text,"Center"}}, {pack_xy,{2,3}} ]),
+
+	gs:create( button, cam_up_button, packer_cam, 
+      [ {label,{text,"Up"}}, {pack_xy,{2,2}} ]),
+
+	gs:create( button, cam_down_button, packer_cam, 
+      [ {label,{text,"Down"}}, {pack_xy,{2,4}} ]),
 	
 	gs:config( packer_cam, [ {width,CameraPanelWidth}, 
 						 {height,CameraPanelHeight} ] ),
@@ -330,8 +708,14 @@ create_initial_gui( GsId ) ->
 	{MainWin,Canvas}.
 
 
-%display_camera_panel( MainWin ) ->
-	
+
+update_camera_panel( GuiState ) ->
+	Camera = get_current_camera( GuiState ),
+	{X,Y} = Camera#camera.location,
+	Text = io_lib:format( "~s camera\nAt [~B;~B]", [Camera#camera.name,X,Y] ),
+	gs:config( cam_label, [ {label,{text,Text}} ] ).
+
+
 
 % Starts the GUI.
 start() ->
@@ -349,6 +733,8 @@ start() ->
 	 },
 
 	render_world( InitialWorld, Canvas ),
+	
+	update_camera_panel( InitialGuiState ),
 
     % Sets the GUI to visible:
 	gs:config( MainWin, {map,true} ),
@@ -365,8 +751,10 @@ map_view_loop( GuiState ) ->
 	
 	receive
         
-		{gs,_Ok,click,_,_} ->
-			io:format( "Click!~n" );
+		{gs,cam_left_button,click,_Data,_Arg} ->
+			io:format( "Click left!~n" ),
+			%CurrentCam = GuiState#gui_state.current_camera,
+			map_view_loop( GuiState );
 
 		{gs,_Pair,destroy,[],[]} ->
 			io:format( "Quitting Orge Map!~n" ),
