68e44c69b4afd0c40a27b7c66059b907cadd1599
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-06-05 19:41:07 +0000

    Origin point added.

--- src/code/servers/functional-services/world-management/src/class_MapSupervisor_test.erl
+++ src/code/servers/functional-services/world-management/src/class_MapSupervisor_test.erl
@@ -8,26 +8,23 @@
 -include("traces_for_tests.hrl").
 
 
-	
+
 % Runs the test.
 run() ->
 
 	?test_start,
 
 	TestBoundaries = { _TopLeft={-1000,2000}, _BottomRight={1500,-700} },
-	
-	TestWorld = class_VirtualWorld:synchronous_new_link( 
+
+	TestWorld = class_VirtualWorld:synchronous_new_link(
 		"My Test Virtual World", TestBoundaries ),
+
+	TestWorld ! {addPoint,[_Coord={250,650}, _Label="Origin" ]},
 
 	MapSupervisorPid = class_MapSupervisor:create_supervisor_for( TestWorld ),
 
-	receive 
-
-		t -> t 
-	
-	end,
+	test_receive(),
 
 	MapSupervisorPid ! delete,
 
 	?test_stop.
-
